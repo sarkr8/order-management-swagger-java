e9db7badec9ff913d2fd31a9020bffa6a9f7c9db
Hiram Martinez Tumalan <hirammartinez@outlook.es>
2026-04-25 23:51:03 -0600

    Initial commit: Spring Boot project structure

---
+++ order-management-swagger/src/main/java/com/order_management_swagger/OrderManagementSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.order_management_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementSwaggerApplication.class, args);
+	}
+
+}
---
+++ order-management-swagger/src/test/java/com/order_management_swagger/OrderManagementSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_management_swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
